aa80303c0365c464da551d54c8276fea8ae6f012
jevoncode <jevoncode@gmail.com>
2014-06-17 14:52:52 +0800

    the first of spring mvc examples, HelloWorld!

---
+++ springmvc-helloworld/src/main/java/com/jevoncode/springmvcexample/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.jevoncode.springmvcexample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/helloWorld")
+public class HelloWorldController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String hello(ModelMap model) {
+		model.addAttribute("name", "JC Hello World!");
+		return "helloWorld";
+	}
+
+}
