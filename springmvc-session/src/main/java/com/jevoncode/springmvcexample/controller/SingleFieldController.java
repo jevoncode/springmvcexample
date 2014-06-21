a5e7c991fd3f728392bcab9a08df450b4f6b0b65
jevoncode <jevoncode@gmail.com>
2014-06-21 23:51:00 +0800

    Spring MVC Session Example

---
+++ springmvc-session/src/main/java/com/jevoncode/springmvcexample/controller/SingleFieldController.java
@@ -0,0 +1,25 @@
+package com.jevoncode.springmvcexample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@SessionAttributes("thought")
+public class SingleFieldController {
+	@RequestMapping(value = "/single-field")
+	public ModelAndView singleFieldPage() {
+		return new ModelAndView("single-field-page");
+	}
+
+	@RequestMapping(value = "/remember")
+	public ModelAndView rememberThought(@RequestParam String thoughtParam) {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("thought", thoughtParam);
+		modelAndView.setViewName("index");
+		return modelAndView;
+	}
+
+}
---
+++ springmvc-session/src/main/webapp/WEB-INF/view/single-field-page.jsp
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>Adding of a String into the session</title>
+</head>
+<body>
+	<h2>Adding of a String into the session</h2>
+
+	<form action="remember.html" method="post">
+		<table>
+			<tbody>
+				<tr>
+					<td>To remember:</td>
+					<td><input name="thoughtParam" type="text"></td>
+				</tr>
+				<tr>
+					<td><input type="submit"></td>
+					<td></td>
+				</tr>
+			</tbody>
+		</table>
+	</form> 
+</body>
+</html>
